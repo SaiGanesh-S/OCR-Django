59d195548c23a767fc3731a14c83b23471e4f5c0
SaiGanesh0509 <saiganesh.sarboj@gmail.com>
2022-10-25 17:37:21 +0530

    updated Security key

--- OCR_Django/OCR/settings.py
+++ OCR_Django/OCR/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u_xl(fl*vlg2+t#@h5(@k#%4u&+q-#_p7c7%a_e$u++=_7n(r5'
+SECRET_KEY = 'type yours'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
